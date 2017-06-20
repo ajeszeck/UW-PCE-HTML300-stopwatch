06a49d0e5aee08f7c9cf49315747f0a58250c36d
Anna J <ajeszeck@gmail.com>
2017-06-20 10:35:51 -0700

    starts adding a lap recording function

--- stopwatch.js
+++ stopwatch.js
@@ -1,3 +1,21 @@
+//button controls
+const start = document.querySelector("button.start")
+const stop = document.querySelector("button.stop")
+const lap = document.querySelector("button.lap")
+//DOM elements that I need to update
+const lapList = document.querySelector("#laplist")
+const stopwatch = document.querySelector("#stopwatch")
+
+//constants that I should never change
+const laps = []
+const intervalRate = 10 //every 10 ms
+
+//values that will change pretty often
+let intervalId = null
+let rawTime = 0
+
+
+
 // turns the time into a human readable format
 function formatTime (raw) {
   let seconds = Math.floor(raw / 1000)
@@ -6,6 +24,46 @@ function formatTime (raw) {
   seconds = seconds - (60 * minutes) + fractionalSeconds
 
   return `${zeroPad(minutes)}:${zeroPad(seconds.toFixed(2))}`
+}
+//starts stopwatch by creating a new intervalId
+//we will store the interval id so we can manipulate interval later
+function stopwatchStart(event) {
+  event.preventDefault()
+  console.log("started")
+
+  //every ten milliseconds, update the stopwatch
+  intervalId = setInterval(stopwatchUpdate, intervalRate)
+}
+
+//stops the stopwatch by clearing the interval
+function stopwatchStop(event) {
+  event.preventDefault()
+  console.log("stopped")
+  //clear the interval
+  clearInterval(intervalId)
+}
+
+//lap function to take a snapshot time when button is clicked
+//adds to lapList div
+function lapRecorder() {
+  event.preventDefault();
+  console.log("lap recorded!");
+  let currentTime = stopwatchTime.innerHTML;
+  laps.push(currentTime);
+  lapList.innerHTML = "";
+
+  for (var i = 0; i < laps.length; i++) {
+    var currentLap = laps[i];
+    var li = document.createElement("li");
+    li.innerHTML = currentLap;
+    lapList.appendChild(li);
+  }
+}
+//adds the interval to the stopwatch time since the last tick.
+//then update the dom with the new stopwatch time
+function stopwatchUpdate() {
+  rawTime += intervalRate
+  stopwatchTime.innerHTML = formatTime(rawTime)
 }
 
 // adds a leading zero because humans like them
@@ -16,4 +74,7 @@ function zeroPad (value) {
 
 document.addEventListener("DOMContentLoaded", function () {
   console.log('ready!')
+  start.addEventListener("click", stopwatchStart)
+  stop.addEventListener("click", stopwatchStop)
+  lap.addEventListener("click", lapRecorder)
 })
